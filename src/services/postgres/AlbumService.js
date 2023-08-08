0b16836847d5f321c27542d07148c65720dc669e
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2023-08-08 10:04:41 +0800

    AddAlbum
    first commit with add Album function

---
+++ src/services/postgres/AlbumService.js
@@ -0,0 +1,30 @@
+/* eslint no-underscore-dangle: 0 */
+
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class AlbumService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = `album-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO album VALUES($1, $2, $3) RETURNING id',
+      values: [id, name, year],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = AlbumService;
